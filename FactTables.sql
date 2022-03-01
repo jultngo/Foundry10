7905e928ebae2adb70700365d924805347a1884f
Julian Ngo <julngo@uw.edu>
2022-02-28 22:33:45 -0800

    finished the rest of the create tables for lookup and fact tables

--- FactTables.sql
+++ FactTables.sql
@@ -10,7 +10,6 @@
 -- Employee
 -- Job
 
--- I think that it might be in our best interested of removing this table and move start/end date to the activity table
 CREATE TABLE ActivityStatus (
     ActivityStatusID int NOT NULL PRIMARY KEY,
     StatusID int NOT NULL FOREIGN KEY REFERENCES Status(StatusID),
@@ -22,44 +21,68 @@ DROP TABLE ActivityStatus;
 
 CREATE TABLE Activity (
     ActivityID int NOT NULL PRIMARY KEY,
-    ProgramID int NOT NULL FOREIGN KEY REFERENCES Program(ProgramID),
-    ActivityID int NOT NULL FOREIGN KEY REFERENCES Activity(ActivityID),
-    ActivityStatusID int NOT NULL FOREIGN KEY REFERENCES ActivityStatus(ActivityStatusID),
+    ActivityTypeID int NOT NULL FOREIGN KEY REFERENCES ActivityType(ActivityTypeID),
     ActivityName VARCHAR(255) NOT NULL
 )
 DROP TABLE Activity;
 
-CREATE TABLE PartnerContact (
-
-)
-DROP TABLE PartnerContact;
-
-CREATE TABLE PrPoc (
-
-)
-DROP TABLE PrPoc;
-
 CREATE TABLE PartnerOrg (
+    PartnerOrgID int NOT NULL PRIMARY KEY,
+    OrgTypeID INT NOT NULL FOREIGN KEY REFERENCES OrgType(OrgTypeID),
+    PartnerOrgName VARCHAR(200),
+    Phone VARCHAR(10), -- PHONE NUMBERS IN THE US ARE 10 DIGITS LONG
+    Address VARCHAR(255),
+    City VARCHAR(255),
+    State VARCHAR(15),
+    Zipcode VARCHAR(10)
 
 )
 DROP TABLE PartnerOrg;
 
-CREATE TABLE Philantropic (
-
+CREATE TABLE PartnerContact (
+    PartnerContactID int NOT NULL PRIMARY KEY,
+    PartnerOrgID int NOT NULL FOREIGN KEY REFERENCES PartnerOrg(PartnerOrgID),
+    PartnerDesignationID int NOT NULL FOREIGN KEY REFERENCES PartnerDesignation(PartnerDesignationID),
+    Fname VARCHAR(25),
+    Lname VARCHAR(25),
+    Phone VARCHAR(10),
+    Email VARCHAR(50)
 )
-DROP TABLE Philantropic;
+DROP TABLE PartnerContact;
 
-CREATE TABLE TeamEmployee (
-
+CREATE TABLE PrPoc (
+    PrPocID INT NOT NULL PRIMARY KEY,
+    PartnerContactID INT NOT NULL FOREIGN KEY REFERENCES PartnerContact(PartnerContactID),
+    ActivityID INT NOT NULL FOREIGN KEY REFERENCES Activity(ActivityID)
 )
-DROP TABLE TeamEmployee;
+DROP TABLE PrPoc;
 
-CREATE TABLE Employee (
-
+CREATE TABLE Philanthropic (
+    PhilanthropicID INT NOT NULL PRIMARY KEY,
+    PartnerOrgID INT NOT NULL FOREIGN KEY REFERENCES ParterOrg(PartnerOrgID),
+    TeamID INT NOT NULL FOREIGN KEY REFERENCES Team(TeamID),
+    SignDate DATE,
+    PhilAmount NUMERIC(15,5),
+    Purpose VARCHAR(255)
 )
-DROP TABLE Employee;
+DROP TABLE Philanthropic;
+
+CREATE TABLE ActivityEmployee (
+    ActivityEmployeeID INT NOT NULL PRIMARY KEY,
+    ActivityID INT NOT NULL FOREIGN KEY REFERENCES Activity(ActivityID),
+    EmployeeID INT NOT NULL FOREIGN KEY REFERENCES Employee(EmployeeID),
+    StartDate DATE,
+    EndDate DATE
+)
+DROP TABLE ActivityEmployee;
 
 CREATE TABLE Job (
-
+    JobID INT NOT NULL PRIMARY KEY,
+    TeamID INT NOT NULL FOREIGN KEY REFERENCES Team(TeamID),
+    EmployeeID INT NOT NULL FOREIGN KEY REFERENCES Employee(EmployeeID),
+    PositionID INT NOT NULL FOREIGN KEY REFERENCES Position(PositionID),
+    StartDate DATE,
+    EndDate DATE,
+    Salary NUMERIC(15,5),
 )
 DROP TABLE Job;
--- LookupTables.sql
+++ LookupTables.sql
@@ -9,47 +9,52 @@
 -- Team
 
 CREATE TABLE Position (
-    PositionID int NOT NULL PRIMARY KEY,
+    PositionID INT NOT NULL PRIMARY KEY,
     PositionName VARCHAR(255) NOT NULL,
     PositionDesc VARCHAR(255),
 )
 DROP TABLE Position;
 
 CREATE TABLE Status (
-    StatusID int NOT NULL PRIMARY KEY,
+    StatusID INT NOT NULL PRIMARY KEY,
     StatusName VARCHAR(255) NOT NULL,
     StatusDesc VARCHAR(255)
 )
 DROP TABLE Status;
 
 CREATE TABLE ActivityType (
-    ActivityTypeID int NOT NULL PRIMARY KEY,
+    ActivityTypeID INT NOT NULL PRIMARY KEY,
     ActivityTypeName VARCHAR(255) NOT NULL,
     ActivityTypeDesc VARCHAR(255)
 )
 DROP TABLE ActivityType;
 
 CREATE TABLE OrgType (
-    OrgTypeID int NOT NULL PRIMARY KEY,
+    OrgTypeID INT NOT NULL PRIMARY KEY,
     OrgTypeName VARCHAR(255) NOT NULL,
     OrgTypeDesc VARCHAR(255)
 )
 DROP TABLE OrgType;
 
 CREATE TABLE PartnerDesignation (
-    PartnerDesignationID int NOT NULL PRIMARY KEY,
+    PartnerDesignationID INT NOT NULL PRIMARY KEY,
     PartnerDesignationName VARCHAR(255) NOT NULL,
     PartnerDesignationDesc VARCHAR(255)
 )
 DROP TABLE PartnerDesignation;
 
 CREATE TABLE Team (
-    TeamID int NOT NULL PRIMARY KEY,
+    TeamID INT NOT NULL PRIMARY KEY,
     TeamName VARCHAR(255) NOT NULL,
     TeamDesc VARCHAR(255)
 )
 DROP TABLE Team;
 
-CREATE FakeTeam(
-    FTID int NOT NULL PRIMARY KEY
+CREATE TABLE Employee (
+    EmployeeID INT NOT NULL PRIMARY KEY,
+    Fname VARCHAR(25),
+    Lname VARCHAR(25),
+    Cell VARCHAR(10),
+    Email VARCHAR(50)
 )
+DROP TABLE Employee;
