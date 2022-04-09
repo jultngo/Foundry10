958b3b3a15c432ecad847340ea6efed9089fed00
Clarissa Kusumo <ckusumo@uw.edu>
2022-04-09 13:22:11 -0700

    Include look-up tables population

--- LookupTables.sql
+++ LookupTables.sql
@@ -63,3 +63,34 @@ CREATE TABLE Employee (
     EmployeeExitReason VARCHAR(255)
 )
 DROP TABLE Employee;
+
+-- LOOK-UP TABLES POPULATION
+INSERT INTO [Position] (PositionName, PositionDesc, PositionBaseSalary)
+VALUES('Associate Researcher', '1-2 years of Research Experience', '60000'),
+('Program Coordinator', '4-7 years of Experience', '80000' ),
+('Project Manager', '3-7 years of Experience', '75000')
+
+INSERT INTO [Employee] (EmployeeFName, EmployeeLName, EmployeePhone, EmployeeEmail, EmployeeHireDate, EmployeeLastDate, EmployeeExitReason)
+VALUES('Luanna', 'Macabee', '2624537467', 'lmacabee@hotmail.com', '2020-12-01', NULL, NULL),
+('Krissy', 'Finey', '3284496839', 'kfiney@hotmail.com', '2019-12-01', '2022-01-01', 'Resign'),
+('Trent', 'Polkis', '4763847788', 'tpolkis@hotmail.com', '2017-11-25', NULL, NULL)
+
+INSERT INTO [Team] (TeamName, TeamDesc)
+VALUES('Research', 'Team that does all of the research'),
+('School', 'Team that works with schools'),
+('Restaurant', 'Team that works with restaurants')
+
+INSERT INTO [Status] (StatusName, StatusDesc)
+VALUES('Initial', 'Initial Phase of an activity'),
+('In Progress', 'On-going activities'),
+('Canceled', 'Activities that are canceled'),
+('Completed', 'Completed Activities')
+
+INSERT INTO [OrgType] (OrgTypeName, OrgTypeDesc)
+VALUES('Corporation', NULL), ('Non-Profit', NULL), ('Government', NULL)
+
+INSERT INTO [ActivityType] (ActivityTypeName, ActivityTypeDesc)
+VALUES('Cognitive Development', NULL), ('Creativity', NULL), ('Technology and Learning', NULL)
+
+INSERT INTO [PartnerPosition] (PartnerPositionName, PartnerPositionDesc)
+VALUES('Account Manager', NULL), ('Senior Account Manager', NULL), ('Business Manager', NULL)
