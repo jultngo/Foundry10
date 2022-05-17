2d71f849746d109c552655417827e3a9a5f6fa07
Clarissa Kusumo <ckusumo@uw.edu>
2022-05-16 22:21:06 -0700

    Finalize update executables

--- UpdateExecutables.sql
+++ UpdateExecutables.sql
@@ -36,35 +36,35 @@ GO
 -- UPDATE ACTIVITY INFORMATION
 -- make sure that the activity and activitytype that need to be updated has exist before running this script
 EXEC UpdateActivityInfo
-@Target_AName = 'Plant a tree', --name of the current activity name, which info will be updated (this is to lookup the current activity info)
-@Target_NumParticipants = '55', -- the number of participants of the current activity, which info will be updated (this is to lookup the current activity info)
-@Target_ActType = 'Social and Cultural Development', --name of the current activity type for the targeted activity info, which info will be updated (this is to lookup current activity info)
-@Update_AName = 'Plant a tree for a better world', --the updated hire activity name if this needs to be updated, if not, fill this information with the current info
-@Update_NumParticipants = '75', --the updated number of participants if this needs to be updated, if not, fill this information with the current info
-@Update_ActType = 'Social and Cultural Development'--the updated activitytype name if this needs to be updated, if not, fill this information with the current info
+@Target_AName = '', --name of the current activity name, which info will be updated (this is to lookup the current activity info)
+@Target_NumParticipants = '', -- the number of participants of the current activity, which info will be updated (this is to lookup the current activity info)
+@Target_ActType = '', --name of the current activity type for the targeted activity info, which info will be updated (this is to lookup current activity info)
+@Update_AName = '', --the updated hire activity name if this needs to be updated, if not, fill this information with the current info
+@Update_NumParticipants = '', --the updated number of participants if this needs to be updated, if not, fill this information with the current info
+@Update_ActType = ''--the updated activitytype name if this needs to be updated, if not, fill this information with the current info
 GO 
 
 -- UPDATE JOB END DATE
 -- make sure that the job that needs to be updated exist before running this script. Also make sure that this info exist in Job Table.
 EXEC UpdateJobEndDate
-@Target_Eemail = 'blightyear@hotmail.com', --name of the current Employee's email, whose info will be updated (this is to lookup the current employee info in the Job table)
-@Target_PName = 'Project Manager', --name of the current Employee's position name, whose info will be updated (this is to lookup the current employee position info in the Job table)
-@Target_TName = 'Research', --name of the current Employee's team name, whose info will be updated (this is to lookup the current employee team info in the Job table)
-@Update_JobEndDate = '2020-01-01'--the updated end date
+@Target_Eemail = '', --name of the current Employee's email, whose info will be updated (this is to lookup the current employee info in the Job table)
+@Target_PName = '', --name of the current Employee's position name, whose info will be updated (this is to lookup the current employee position info in the Job table)
+@Target_TName = '', --name of the current Employee's team name, whose info will be updated (this is to lookup the current employee team info in the Job table)
+@Update_JobEndDate = ''--the updated end date
 GO 
 
 -- UPDATE ACTIVITY EMPLOYEE END DATE 
 -- make sure that the activity and employee exist before running this script. Also make sure that this info exist in ActivityEmployee Table.
 EXEC UpdateActEmployeeEndDate
-@Target_AName = 'Elevator pitch webinar', --name of the current activity name, which info will be updated (this is to lookup the current activity info in the ActivityEmployee table)
-@Target_Eemail = 'blightyear@hotmail.com',  --name of the current Employee's email, whose info will be updated (this is to lookup the current employee info in the ActivityEmployee table)
-@Update_EndDate = '2022-04-01' --the updated end date for this employee for this specific activity
+@Target_AName = '', --name of the current activity name, which info will be updated (this is to lookup the current activity info in the ActivityEmployee table)
+@Target_Eemail = '',  --name of the current Employee's email, whose info will be updated (this is to lookup the current employee info in the ActivityEmployee table)
+@Update_EndDate = '' --the updated end date for this employee for this specific activity
 GO 
 
 -- UPDATE ACTIVITY STATUS END DATE
 -- make sure that the status and activity name exist before running this script. Also make sure that this info exist in ActivityStatus Table
 EXEC UpdateActStatusEndDate
-@Target_SName = 'Initial', --name of the current status name, which info will be updated (this is to lookup the current activity info in the ActivityStatus table)
-@Target_AName = 'Elevator pitch webinar', --name of the current activity name, which info will be updated (this is to lookup the current activity info in the ActivityStatus table)
-@Update_EndDate = '2022-04-15' --the updated end date for this status for this specific activity
+@Target_SName = '', --name of the current status name, which info will be updated (this is to lookup the current activity info in the ActivityStatus table)
+@Target_AName = '', --name of the current activity name, which info will be updated (this is to lookup the current activity info in the ActivityStatus table)
+@Update_EndDate = '' --the updated end date for this status for this specific activity
 GO 
